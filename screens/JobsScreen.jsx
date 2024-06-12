b751b8a89632370abd04b3b190aae01154992fd4
FaisalMohammedElorm <mohammedelormf@gmail.com>
2024-06-12 22:39:42 +0100

    Jobs

---
+++ screens/JobsScreen.jsx
@@ -0,0 +1,14 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+const JobsScreen = () => {
+  return (
+    <View>
+      <Text>JobsScreen</Text>
+    </View>
+  )
+}
+const styles = StyleSheet.create({
+  
+});
+export default JobsScreen
