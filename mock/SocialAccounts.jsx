81d9e20fc70d48227a1d95d2aaf0e432917fdceb
FaisalMohammedElorm <mohammedelormf@gmail.com>
2024-06-13 23:13:14 +0100

    flatlist and images

---
+++ mock/SocialAccounts.jsx
@@ -0,0 +1,74 @@
+export const socialData = [
+  {
+    id:0,
+    image:require("../assets/Vector.png"),
+    job: "Software Engineer",
+    socialAccount: "Facebook",
+    salary: "$180.00",
+    location: "Accra, Ghana"
+
+  },
+  {
+    id:1,
+    image:require("../assets/grommet-icons_google.png"),
+    job: "Technical Writer",
+    socialAccount: "Google",
+    salary: "$160.00",
+    location: "Accra, Ghana"
+
+  },
+  {
+    id:2,
+    image:require("../assets/Vector.png"),
+    job: "Prompt Engineer",
+    socialAccount: "Facebook",
+    salary: "$200.00",
+    location: "Accra, Ghana"
+
+  },
+  {
+    id:3,
+    image:require("../assets/Vector.png"),
+    job: "Backend Developer",
+    socialAccount: "Facebook",
+    salary: "$500.00",
+    location: "Accra, Ghana"
+
+  },
+  {
+    id:4,
+    image:require("../assets/Vector.png"),
+    job: "Cybersecurity Analyst",
+    socialAccount: "Facebook",
+    salary: "$350.00",
+    location: "Accra, Ghana"
+
+  },
+  {
+    id:5,
+    image:require("../assets/Vector.png"),
+    job: "Web Developer",
+    socialAccount: "Facebook",
+    salary: "$700.00",
+    location: "Accra, Ghana"
+
+  },
+  {
+    id:6,
+    image:require("../assets/Vector.png"),
+    job: "UI/UX Designer",
+    socialAccount: "Facebook",
+    salary: "$1,000.00",
+    location: "Accra, Ghana"
+
+  },
+  {
+    id:7,
+    image:require("../assets/Vector.png"),
+    job: "Data Analyst",
+    socialAccount: "Facebook",
+    salary: "$400.00",
+    location: "Accra, Ghana"
+
+  },
+];
--- screens/JobsScreen.jsx
+++ screens/JobsScreen.jsx
@@ -1,14 +1,168 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Image, TextInput, TouchableOpacity, FlatList } from 'react-native'
 import React from 'react'
+import { AntDesign } from '@expo/vector-icons';
+import { socialData } from '../mock/SocialAccounts';
 
 const JobsScreen = () => {
   return (
-    <View>
-      <Text>JobsScreen</Text>
-    </View>
+    <View style={styles.container}>
+    {/** Welcome Section */}
+      <View style={styles.salutationContainer}>
+        <View style={{flexDirection:"column"}}>
+          <Text style={styles.salutationText}>Eric Atsu</Text>
+          <Text style={{color:"#95969D", fontSize:20}}>eric@gmail.com</Text>
+        </View>
+        <View style={{marginLeft:185}}>
+          <Image source={require("../assets/Profile.png")}/>
+        </View>
+      </View>
+      <View style={{marginTop:35}}>
+          
+            <View style={styles.inputContainer}>
+                <View style={{flexDirection:"row"}}>
+                  <TouchableOpacity>
+                    <AntDesign name="search1" size={25} color="black" style={styles.icon} />
+                  </TouchableOpacity>
+                    <TextInput
+                      style={styles.input}
+                        placeholder="Search a job or position"
+                    />
+                    </View>
+                  <TouchableOpacity style={styles.filterIconContainer}>
+                    <Image source={require("../assets/filter5.png")} style={styles.filterIcon}/>
+                  </TouchableOpacity>
+              </View>
+        </View>
+        <View style={{marginTop:40}}>
+          <View style={{flexDirection:"row"}}>
+            <Text style={{fontSize:25, fontWeight:"700"}}> Featured Jobs</Text>
+            <Text style={{marginLeft:170, fontSize:20, color:"#95969D"}}>See all</Text>
+          </View>
+        </View>
+        <View>
+            <FlatList
+              data={socialData}
+                renderItem={({ item }) => (
+                  
+                    <View style={styles.socialDataContainer}>
+                        <View style={{flexDirection:"row"}}>
+                            <View style={{backgroundColor:"white", width:50, borderRadius:5}}>
+                                <Image style={{alignSelf:"center"}}source={item.image}/>
+                            </View>
+
+                              <View style={{flexDirection:"column", alignItems:"center", marginLeft:5}}>
+                                  <View>
+                                      <Text 
+                                        style={{color:"#fff", fontWeight:"600", fontSize:20}}>{item.job}</Text>
+                                  </View>
+
+                                  
+                                  <View>
+                                      <Text style={{color:"#fff", fontWeight:"600"}}>{item.socialAccount}</Text>
+                                  </View>
+                              </View>
+                          </View>
+                        <View style={{flexDirection:"row", marginTop:120, justifyContent:"space-between"}}>
+                            <View>
+                                <Text style={{color:"#fff", fontWeight:"600", fontSize:15}}>{item.salary}</Text>
+                            </View>
+
+                          
+                            <View>
+                                <Text style={{color:"#fff", fontWeight:"600", fontSize:15}}>{item.location}</Text>
+                            </View>
+                      </View>
+                        
+                        
+
+                        
+
+
+
+                        </View>
+                  )}
+                
+                
+                keyExtractor={(item) => item.id.toString()}
+                  horizontal
+                    showsHorizontalScrollIndicator = {false}
+            />
+        </View>
+      </View>
+        
+         
+        
+    
   )
 }
 const styles = StyleSheet.create({
-  
-});
-export default JobsScreen
+  container:{
+    flex:1,
+    margin: 20,
+    },
+  salutationContainer:{
+    marginTop:30,
+    flexDirection:"row"
+
+    },
+  salutationText:{
+    fontSize:30,
+    fontWeight:"500"
+    },
+  inputContainer:{
+    borderWidth:1.5,
+    borderColor:"gray",
+    height:50,
+    borderRadius: 30,
+    width:"80%",
+    alignItems:"center",
+    textAlign:"center",
+    paddingHorizontal:10,
+    flexDirection:"row",
+    
+    },
+    input:{
+      flex:1,
+      fontSize:20,
+      fontWeight:"700",
+       justifyContent:"space-evenly"
+    },
+    icon:{
+      marginRight:20
+    },
+    filterIconContainer:{
+     
+     
+     height:50,
+     width:50,
+     marginLeft:40,
+     justifyContent:"flex-end",
+     alignItems:"center",
+     borderRadius:15,
+     borderWidth:1,
+     borderColor:"gray"
+     
+    },
+    filterIcon:{
+      height:39,
+      width:40,
+      
+    },
+    socialDataContainer:{
+      backgroundColor:"blue", 
+      height:205, 
+      width:250,
+      backgroundColor:"#241a82",
+      margin:20,
+      borderRadius:20,
+      padding:20
+    }
+  });
+  export default JobsScreen
+    
+    
+   
+               
+                  
+            
+          
